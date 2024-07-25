ab455b04368dd655e32391196c7db40d4aeeddc8
Vasyan361 <vasyagromak@yandex.ru>
2024-07-25 23:56:12 +0300

    cleanup code

--- GetMacAddress/GetMacAddress.ino
+++ GetMacAddress/GetMacAddress.ino
@@ -5,7 +5,7 @@ void setup() {
 }
 
 void loop() {
-    Serial.print("ESP Board MAC Address: "); 
+    Serial.print("ESP Board MAC Address: ");
     Serial.println(WiFi.macAddress());
     delay(1000);
 }
